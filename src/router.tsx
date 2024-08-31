c0957d535d1bab562238e4ea80ffa2731810d628
Yogita Shete <yogitashete24@gmail.com>
2024-08-31 17:47:35 +0530

    add auth layout

--- src/router.tsx
+++ src/router.tsx
@@ -1,35 +1,40 @@
-import { createBrowserRouter } from "react-router-dom";
-import LoginPage from "@/pages/LoginPage";
-import HomePage from "@/pages/HomePage";
-import RegisterPage from "./pages/RegisterPage";
-import DashboardLayout from "./layouts/DashboardLayout";
-import BooksPage from "./pages/BooksPage";
+import { createBrowserRouter } from 'react-router-dom';
+import LoginPage from '@/pages/LoginPage';
+import HomePage from '@/pages/HomePage';
+import RegisterPage from './pages/RegisterPage';
+import DashboardLayout from './layouts/DashboardLayout';
+import BooksPage from './pages/BooksPage';
+import AuthLayout from './layouts/AuthLayout';
 
 const router = createBrowserRouter([
-  {
-    path: "dashboard",
-    element: <DashboardLayout />,
-    children: [
-      {
-        path: "home",
-        element: <HomePage />,
-      },
-
-      {
-        path: "books",
-        element: <BooksPage />,
-      },
-    ],
-  },
-
-  {
-    path: "/login",
-    element: <LoginPage />,
-  },
-
-  {
-    path: "/register",
-    element: <RegisterPage />,
-  },
+    {
+        path: 'dashboard',
+        element: <DashboardLayout />,
+        children: [
+            {
+                path: 'home',
+                element: <HomePage />,
+            },
+            {
+                path: 'books',
+                element: <BooksPage />,
+            },
+        ],
+    },
+    {
+        path: 'auth',
+        element: <AuthLayout />,
+        children: [
+            {
+                path: 'login',
+                element: <LoginPage />,
+            },
+            {
+                path: 'register',
+                element: <RegisterPage />,
+            },
+        ],
+    },
 ]);
+
 export default router;
