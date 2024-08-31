f3bc1e05dfcc8f2862cb19649a414db009325cfe
Yogita Shete <yogitashete24@gmail.com>
2024-08-31 11:54:53 +0530

    add router dom

---
+++ src/router.tsx
@@ -0,0 +1,17 @@
+import { createBrowserRouter } from "react-router-dom";
+import HomePage from "@/pages/HomePage";
+import LoginPage from "@/pages/LoginPage";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage/>
+  },
+  {
+    path: "/login",
+    element: <LoginPage/>
+  },
+]);
+
+export default router;
