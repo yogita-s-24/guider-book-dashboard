de7290bef7a00dd1271360f6972ac4c1df1b5525
Yogita Shete <yogitashete24@gmail.com>
2024-09-08 23:23:44 +0530

    make routes protected

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,7 +1,14 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import useTokenStore from '@/store';
+import { Navigate, Outlet } from 'react-router-dom';
 
 const AuthLayout = () => {
+
+  const token = useTokenStore((state) => state.token);
+
+    if (token) {
+        return <Navigate to={'/dashboard/home'} replace />;
+    }
+    
   return (
     <>
     <Outlet/>
--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -25,9 +25,17 @@ import {
   Package2,
   Search,
 } from "lucide-react";
-import { Link, Outlet } from "react-router-dom";
+import { Link, Navigate, Outlet } from "react-router-dom";
+import useTokenStore from "@/store";
 
 const DashboardLayout = () => {
+
+  const token = useTokenStore((state) => state.token);
+
+    if (token === '') {
+        return <Navigate to={'/auth/login'} replace />;
+    }
+    
   return (
     <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
       <div className="hidden border-r bg-muted/40 md:block">
