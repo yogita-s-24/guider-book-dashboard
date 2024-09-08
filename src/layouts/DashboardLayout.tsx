781877b7aef2456177e0d60cf7420a0b7b1fcb0d
Yogita Shete <yogitashete24@gmail.com>
2024-09-08 23:29:49 +0530

    add logout functionality

--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -29,13 +29,17 @@ import { Link, Navigate, Outlet } from "react-router-dom";
 import useTokenStore from "@/store";
 
 const DashboardLayout = () => {
+  const { token, setToken } = useTokenStore((state) => state);
 
-  const token = useTokenStore((state) => state.token);
+  if (token === "") {
+    return <Navigate to={"/auth/login"} replace />;
+  }
 
-    if (token === '') {
-        return <Navigate to={'/auth/login'} replace />;
-    }
-    
+  const logout = () => {
+    console.log("Logging out!");
+    setToken("");
+  };
+
   return (
     <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
       <div className="hidden border-r bg-muted/40 md:block">
@@ -156,7 +160,11 @@ const DashboardLayout = () => {
               <DropdownMenuItem>Settings</DropdownMenuItem>
               <DropdownMenuItem>Support</DropdownMenuItem>
               <DropdownMenuSeparator />
-              <DropdownMenuItem>Logout</DropdownMenuItem>
+              <DropdownMenuItem>
+                <Button onClick={logout} variant={"link"}>
+                  Logout
+                </Button>
+              </DropdownMenuItem>{" "}
             </DropdownMenuContent>
           </DropdownMenu>
         </header>
